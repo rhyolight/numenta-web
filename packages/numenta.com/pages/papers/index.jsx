ac4d590a6b00a428159aeab66b1e8c2087f04267
Luiz Scheinkman <lscheinkman@numenta.com>
2017-10-16 14:59:46 -0700

    WEB-624: Add images to paper list

--- packages/numenta.com/pages/papers/index.jsx
+++ packages/numenta.com/pages/papers/index.jsx
@@ -6,6 +6,8 @@ import capitalize from 'lodash/capitalize'
 import Helmet from 'react-helmet'
 import React from 'react'
 
+import Image from 'numenta-web-shared-components/lib/Image'
+import ImageLink from 'numenta-web-shared-components/lib/ImageLink'
 import ListItem from 'numenta-web-shared-components/lib/ListItem'
 import ListOrder from 'numenta-web-shared-components/lib/ListOrder'
 import Paragraph from 'numenta-web-shared-components/lib/Paragraph'
@@ -29,31 +31,53 @@ const PapersPage = (props, {route}) => {
   const items = posts.sort(sortOrderAscend).map(({data, file, path}) => {
     const categoryNice = capitalize(data.category.replace(/-/, ' '))
     const url = (data.type === 'link') ? data.link : path
+    let media = null
+    if (data.image) {
+      media = (
+        <ImageLink to={url}>
+          <Image
+            alt={data.title}
+            border={true}
+            respond="mw"
+            shadow={true}
+            src={`${path}${data.image}`}
+          />
+        </ImageLink>
+      )
+    }
     return (
       <ListItem key={file.stem}>
         <div className={styles.paper}>
-          <div className={styles.title}>
-            <TextLink to={url}>
-              {data.title}
-            </TextLink>
+          <div className={styles.columns}>
+            <div className={styles.aside}>
+              {media}
+            </div>
+            <div className={styles.content}>
+              <div className={styles.title}>
+                <TextLink to={url}>
+                  {data.title}
+                </TextLink>
+              </div>
+              <div className={styles.meta}>
+                <Subtle>
+                  {data.author}
+                  <Spacer />
+                  {categoryNice}
+                  <br />
+                  {data.org}
+                  <Spacer />
+                  {data.date}
+                </Subtle>
+              </div>
+              <Paragraph>
+                {data.brief}
+                <br />
+                <span className={styles.remarks}>
+                  {data.remarks}
+                </span>
+              </Paragraph>
+            </div>
           </div>
-          <div className={styles.meta}>
-            <Subtle>
-              {data.author}
-              <Spacer />
-              {categoryNice}
-              <br />
-              {data.org}
-              <Spacer />
-              {data.date}
-            </Subtle>
-          </div>
-          <Paragraph>
-            {data.brief}
-            <br /><span className={styles.remarks}>
-              {data.remarks}
-            </span>
-          </Paragraph>
         </div>
       </ListItem>
     )
