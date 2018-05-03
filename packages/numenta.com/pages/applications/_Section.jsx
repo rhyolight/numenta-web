7eecac3852a4aca7cbdf87416ab6d27c1ba12928
cmaver <cmaver@numenta.com>
2018-05-03 16:58:23 -0700

    Test of indenting

--- packages/numenta.com/pages/applications/_Section.jsx
+++ packages/numenta.com/pages/applications/_Section.jsx
@@ -33,7 +33,7 @@ const SectionApplications = (props, {config}) => {
       <div className={styles.columns}>
         <div className={styles.aside}>
           <Image
-            alt="HTM Applications"
+            alt="     HTM Applications"
             respond="mw"
             src={ImageApps}
           />
